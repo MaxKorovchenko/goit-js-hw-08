f0014d4259d1fe59d54d774b21e5cc309416b63f
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-20 17:59:48 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,28 @@
 import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import lightboxCss from 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
 console.log(SimpleLightbox);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
