46cd904343d71b0769a7323e61caab5213b01654
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-21 17:37:55 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,14 @@
 import Player from '@vimeo/player';
-console.log(Player);
-const a = 15;
-console.log(a);
+var throttle = require('lodash.throttle');
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// console.log(player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
 
-// const onPlay = function (data) {
-//   const localStorage = data.duration;
-//   return localStorage;
-// };
+const onPlay = function ({ seconds }) {
+  return localStorage.setItem(CURRENT_TIME, seconds);
+};
 
-// player.on('timeupdate', onPlay);
-// ('videoplayer-current-time');
-// player.setCurrentTime(localStorage);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
