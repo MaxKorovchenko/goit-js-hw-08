74b74e6c6fded5b25a03ee743f81e8e0a4b97dc4
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-21 21:45:48 +0200

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
 
 const onPlay = function ({ seconds }) {
-  return localStorage.setItem(CURRENT_TIME, seconds);
+  return localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
