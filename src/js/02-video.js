ba26693b98171dec658b8692a376f7a84b2db768
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-21 10:03:49 +0200

    update task-01

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,17 @@
 import Player from '@vimeo/player';
-
 console.log(Player);
+const a = 15;
+console.log(a);
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// console.log(player);
+
+// const onPlay = function (data) {
+//   const localStorage = data.duration;
+//   return localStorage;
+// };
+
+// player.on('timeupdate', onPlay);
+// ('videoplayer-current-time');
+// player.setCurrentTime(localStorage);
