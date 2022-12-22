8ed236ca25668cd35c69440f914c90f346455d54
MaxKorovchenko <max.korovchenko@gmail.com>
2022-12-22 18:03:26 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const FORM_MESSAGE = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(FORM_MESSAGE, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+
+  const savedData = localStorage.getItem(FORM_MESSAGE);
+  console.log(JSON.parse(savedData));
+
+  localStorage.clear();
+}
+
+function updateFormFields() {
+  const getData = localStorage.getItem(FORM_MESSAGE);
+  const getDataParsed = JSON.parse(getData);
+
+  if (getData) {
+    form.elements.email.value = getDataParsed.email;
+    form.elements.message.value = getDataParsed.message;
+  }
+}
+updateFormFields();
